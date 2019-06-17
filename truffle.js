2a0d966daed796555ad792ed69ea78fa42c20af5
Adan Carreno <adancs+pal@gmail.com>
2019-06-17 02:26:52 -0300

    Adding Ropsten config

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -18,6 +21,14 @@ module.exports = {
                port: 8545, 
                network_id: 4, // Rinkeby official network id
                gas: 4700000
-          }
+          }, 
+          ropsten: {
+               provider: function () {
+                    return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
+               },
+               network_id: 3,
+               gas: 4500000,
+               gasPrice: 10000000000
+      }
      }
 };
